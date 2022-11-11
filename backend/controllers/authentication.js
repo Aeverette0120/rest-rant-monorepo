e757568808e5dc83ef30e5958a59c6fdee80a7f6
Aaron Everette <aeverette0120@gmail.com>
2022-11-10 19:54:03 -0500

    madechanges

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,27 +1,40 @@
-const router = require('express').Router()
-const db = require('../models')
-const bcrypt = require('bcrypt')
+const router = require("express").Router();
+const db = require("../models");
+const bcrypt = require("bcrypt");
 
-const { User } = db
+const { User } = db;
 
-
-router.post('/', async (req, res) => {
-const { email, password } = req.body
-    let user = await User.findOne({
-        where: { email }
+router.post("/", async (req, res) => {
+  const { email, password } = req.body;
+  let user = await User.findOne({
+    where: { email },
+  });
+  if (!user || !(await bcrypt.compare(password, user.passwordDigest))) {
+    res.status(404).json({
+      message: "We cannot find this user with provided email and password",
     });
-    if(!user || !await bcrypt.compare(password, user.passwordDigest)) {
-        res.status(404).json({
-            message: 'We cannot find this user with provided email and password'
-        })
-        console.log('it did not work')
-    } else {
-        res.json({user})
-    }
+    console.log("it did not work");
+  } else {
+    req.session.userId = user.userId;
 
-    console.log(user)
-})
-  
+    res.json({ user });
+  }
 
+  console.log(user);
+});
 
-module.exports = router
+router.get("/profile", async (req, res) => {
+  console.log('request session userId should be this'+ req.session.userId);
+  try {
+    let user = await User.findOne({
+      where: {
+        userId: req.session.userId, //TODO currect the user ID with cookie ID
+      },
+    });
+    res.json(user);
+  } catch (e) {
+    res.json(null);
+  }
+});
+
+module.exports = router;
